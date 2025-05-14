80566dff7b8611394383bbc7b9adb5516c90f1c0
Joey McKenzie <joey.mckenzie27@gmail.com>
2025-05-14 10:27:29 -0700

    chore: ci cleanup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use Override;
 
 final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
      */
+    #[Override]
     public function register(): void
     {
         //
@@ -21,6 +29,39 @@ final class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        self::configureModels();
+        self::configureVite();
+        self::configureSchema();
+        self::configureCommands();
+        self::configureDates();
+    }
+
+    private function configureModels(): void
+    {
+        Model::automaticallyEagerLoadRelationships();
+        Model::unguard();
+        Model::shouldBeStrict();
+    }
+
+    private function configureVite(): void
+    {
+        Vite::useAggressivePrefetching();
+    }
+
+    private function configureSchema(): void
+    {
+        URL::forceScheme('https');
+    }
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->environment('production') === true,
+        );
+    }
+
+    private function configureDates(): void
+    {
+        Date::use(CarbonImmutable::class);
     }
 }
