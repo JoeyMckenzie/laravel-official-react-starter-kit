aa06aef034a260316a7b01abb0bc8442f0f4b281
Joey McKenzie <joey@givebutter.com>
2025-09-16 07:52:36 -0700

    chore: update rector config

--- rector.php
+++ rector.php
@@ -8,9 +8,10 @@ use RectorLaravel\Set\LaravelSetProvider;
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
+        __DIR__.'/bootstrap/app.php',
+        __DIR__.'/bootstrap/providers.php',
         __DIR__.'/config',
-        __DIR__.'/public',
+        __DIR__.'/public/index.php',
         __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
