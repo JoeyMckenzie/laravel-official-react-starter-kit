5e8631ebea68a640a3d4577de44e276a8e22501f
Joey McKenzie <joey@givebutter.com>
2025-10-20 10:48:44 -0700

    chore: run prettier

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -99,7 +99,7 @@ export default function Login({
                         </div>
 
                         {canRegister ? (
-                            <div className="text-muted-foreground text-center text-sm">
+                            <div className="text-center text-sm text-muted-foreground">
                                 Don&apos;t have an account?{' '}
                                 <TextLink href={register()} tabIndex={5}>
                                     Sign up
